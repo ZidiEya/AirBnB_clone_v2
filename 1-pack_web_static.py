e95a1f353747fb14a09be34d1098cd5388ac873e
ZidiEya <3805@holberton.com>
2022-07-04 22:38:58 +0000

    Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ 
-script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack 
+script that generates a .tgz archive from the contents of 
+the web_static folder of your AirBnB Clone repo, using the function do_pack 
 """
 import datetime
 import os
