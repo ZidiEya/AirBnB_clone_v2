6d390fbf8106caf17d74dc070e64125c7fab3d10
ZidiEya <3805@holberton.com>
2022-07-05 01:30:29 +0000

    script that starts a Flask web application:

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
 """script that starts a Flask web application """
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index():
-    """returning Hello HBNB!"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returning HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
+def c(text):
     """ followed by value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+    return ('C ' + text.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python')
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """ followed by  value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+def python_(text='is cool'):
+    """", followed by value of text variable"""
+    return ('Python ' + text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
-    """n is a  only if n is an integer"""
-    return "{:d} is a number".format(n)
+def number_(n):
+    """n is a  only if n = a integer"""
+    return '{:d} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbersandtemplates(n):
-    """display the  HTML page only if n is an integer"""
-    return render_template('5-number.html', n=n)
+def template_(n):
+    """ display the HTML page only if n is an integer"""
+    return render_template("5-number.html", n=n)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')numberdisplay Python display C display 
+    app.run()numberdisplay Python display C display 
